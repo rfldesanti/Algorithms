3d56cd4e27e17010dd1744fca72e54623c6bb12a
rfldesanti <rafaelsr2018@gmail.com>
2021-02-24 22:27:35 -0300

    Put id #bloco_quadro on <p> tag and started to styling it

--- Javascirpt/BinarySearchAlgorithm/scripts/main.js
+++ Javascirpt/BinarySearchAlgorithm/scripts/main.js
@@ -1,6 +1,6 @@
 let myButton_run = document.querySelector(".run");
 let myButton_reset = document.querySelector(".reset");
-let myText = document.getElementById("text");
+let myText = document.getElementById("bloco_quadro");
 
 function message() {
         if (myText.textContent == '') {
