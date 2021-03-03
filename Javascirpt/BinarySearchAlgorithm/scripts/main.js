2f54f8c609422e9140470e76c0b5abd950ce5626
rfldesanti <rafaelsr2018@gmail.com>
2021-03-02 23:21:12 -0300

    Put a variable to count the attempts to find the target value and changed the return valur to an array

--- Javascirpt/BinarySearchAlgorithm/scripts/main.js
+++ Javascirpt/BinarySearchAlgorithm/scripts/main.js
@@ -1,4 +1,10 @@
-
+/*Improvements to be made on long term:
+    1 - Put options to the user choose:
+        1.1 - the length of the array;
+        1.2 - the fill of the array;
+        1.3 - the value to be searched;
+    2 - Create table for each test made; each time a test is run, create a table;
+*/
 
 let myButton_run = document.querySelector(".run");
 let myButton_reset = document.querySelector(".reset");
@@ -6,6 +12,7 @@ let array = document.getElementById("array");
 let array_len = document.getElementById("array_len");
 let target_value = document.getElementById("target");
 let index = document.getElementById("index");
+let count = document.getElementById("attempts");
 
 myButton_run.onclick = function() {
         arr1 = [1,2,3,4,5,6,7,8,9,10];
@@ -14,7 +21,9 @@ myButton_run.onclick = function() {
         array.textContent = arr1;
         array_len.textContent = len_arr1;
         target_value.textContent = item_arr;
-        index.textContent = binary_search(arr1, item_arr);
+        results = binary_search(arr1, item_arr);
+        index.textContent = results[0];
+        count.textContent = results[1];
 }
 
 myButton_reset.onclick = function() {
@@ -27,18 +36,21 @@ myButton_reset.onclick = function() {
 function binary_search(list, item) {
         var low_item = 0;
         var high_item = list.length - 1;
-
+        var cont = 0;
+        var middle_cont = [];
+        
         while (low_item <= high_item) {
                 middle = Math.floor((low_item + high_item)/2);
                 guess = list[middle];
-                console.log(guess);
+                cont = cont + 1;
+                console.log(cont);
                 if (guess == item) {
-                        return middle;
+                        return middle_cont = [middle, cont];
                 } else if (guess > item) {
                         high_item = middle - 1;
                 } else {
                         low_item = middle + 1;
                 }
         }
-return "NOT IN ARRAY";
+return not_in_array = ["NOT IN ARRAY", cont];
 }
