699a2e9b7da384f32bdec05fab0f3f4b86ef29bf
rfldesanti <rafaelsr2018@gmail.com>
2021-03-03 23:17:41 -0300

    Put while loop

--- Java/BinarySearchAlgorithm.java
+++ Java/BinarySearchAlgorithm.java
@@ -3,7 +3,12 @@ class BinarySearchAlgorithm {
     int[] BinarySearchAlgorithm(list, item) {
 
         int low_value, attempts;
-        int high_value = list.length;
-        
+        int high_value = list.length - 1;
+
+        while (low_value <= high_value) {
+            attempts++;
+            int middle = Integer.parseint((low_value + high_value)/2);
+            
+        }
     }
 }
