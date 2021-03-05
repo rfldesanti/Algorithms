35dda88140e67396e8e916f1251a12ff277c5920
rfldesanti <rafaelsr2018@gmail.com>
2021-03-04 23:09:59 -0300

    Test file working! The logic is OK and code is working!

--- Java/BinarySearchAlgorithm/test_BinarySearchAlgorithm.java
+++ Java/BinarySearchAlgorithm/test_BinarySearchAlgorithm.java
@@ -3,11 +3,12 @@ class test_BinarySearchAlgorithm {
     public static void main(String[] args) {
 
         int[] list = {1,2,3,4,5,6,7,8,9,10,11,12};
-        int item = 6;
+        int item = 12;
         int[] results;
         BinarySearchAlgorithm bsa = new BinarySearchAlgorithm();
         results = bsa.BinarySearchAlgorithm(list, item);
         System.out.println("Value 1 is " + results[0]);
         System.out.println("Value 2 is " + results[1]);
+
     }
 }
