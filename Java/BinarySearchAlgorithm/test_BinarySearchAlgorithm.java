a34ed6ceb3550659b9b791248964413f45cb8c41
rfldesanti <rafaelsr2018@gmail.com>
2021-03-04 23:50:26 -0300

    Test some values and finalize the logic

--- Java/BinarySearchAlgorithm/test_BinarySearchAlgorithm.java
+++ Java/BinarySearchAlgorithm/test_BinarySearchAlgorithm.java
@@ -3,7 +3,7 @@ class test_BinarySearchAlgorithm {
     public static void main(String[] args) {
 
         int[] list = {1,2,3,4,5,6,7,8,9,10,11,12};
-        int item = 9;
+        int item = 12;
         int[] results;
         BinarySearchAlgorithm bsa = new BinarySearchAlgorithm();
         results = bsa.BinarySearchAlgorithm(list, item);
