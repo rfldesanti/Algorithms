c50211882ad2da1b495ed3ce8ff0166e843cf535
rfldesanti <rafaelsr2018@gmail.com>
2021-03-13 23:54:06 -0300

    testing values on java code

--- Java/BinarySearchAlgorithm/test_BinarySearchAlgorithm.java
+++ Java/BinarySearchAlgorithm/test_BinarySearchAlgorithm.java
@@ -5,7 +5,7 @@ class test_BinarySearchAlgorithm {
     public static void main(String[] args) {
 
         int[] list = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
-        int item = 0;
+        int item = 2;
         int[] results;
         BinarySearchAlgorithm bsa = new BinarySearchAlgorithm();
         results = bsa.BinarySearchAlgorithm(list, item);
